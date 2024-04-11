14995caaeb5ca249554dfea8dc0cd220e08bef26
Eugene Tay <94125987+eugenetayyj@users.noreply.github.com>
2024-04-11 14:44:30 -0400

    axios implementation

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import { Paper } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [routingForm, setRoutingForm] = useState("")
+  const [showData, setShowData] = useState(false)
+  const [bankData, setBankData] = useState({})
 
   const sendRequest = async () => {
-
+    const url = "https://uvz9pac8ik.execute-api.us-east-2.amazonaws.com/dev/bank";
+    const body = `?instNum=${routingForm}`;
+    const response = await axios.get(url + body);
+    console.log(response)
+    if (response.status === 200) {
+      setBankData({
+        bankCode: response.data.Item.instCode,
+        bankName: response.data.Item.instName,
+      });
+      setShowData(true)
+    }
   }
-
-  useEffect(() => {
-    console.log(routingForm)
-  })
 
   return (
     <div className="App">
@@ -37,13 +46,18 @@ function App() {
               backgroundColor: "green",
               color: "white",
             }}
+            onClick={sendRequest}
           >
             Validate
           </Button>
         </Paper>
+        {showData && <Paper>
+          <div>Bank Code: {bankData.bankCode}</div>
+          <div>Bank Name: {bankData.bankName}</div>
+        </Paper>}
       </header>
     </div>
   );
-}
+} 
 
 export default App;
