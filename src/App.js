04fe8a2025b08d202f69c708f10ec89847dd70f4
Eugene Tay <94125987+eugenetayyj@users.noreply.github.com>
2024-04-10 23:13:26 -0400

    css updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Paper } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [routingForm, setRoutingForm] = useState("")
+
+  const sendRequest = async () => {
+
+  }
+
+  useEffect(() => {
+    console.log(routingForm)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Canadian Bank Routing Number Look up</div>
+        <Paper elevation={10} className="paper">
+          <TextField
+            id="outlined-basic"
+            label="Routing Number"
+            variant="outlined"
+            onChange={ (event) => setRoutingForm(event.target.value) }
+            style={{
+              position: "flex",
+              width: "80%",
+              marginRight: "5px",
+            }}
+          />
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "green",
+              color: "white",
+            }}
+          >
+            Validate
+          </Button>
+        </Paper>
       </header>
     </div>
   );
